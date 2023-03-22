1bbd35b2121dd473d808529a3cefdbec83fc1aec
任孟雪 <renmengxue@jqrjl.com>
2023-03-22 14:26:01 +0800

    update：首页，查找，订单，我的四个页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,62 @@
+/*
+路由器模块
+ */
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+import Msite from '../pages/Msite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/',
+    redirect: '/msite'
+  },
+  {
+    path: '/msite',
+    component: Msite,
+  },
+  {
+    path: '/search',
+    component: Search,
+  },
+  {
+    path: '/order',
+    component: Order,
+  },
+  {
+    path: '/profile',
+    component: Profile,
+  }
+]
+// const originalPush = VueRouter.prototype.push
+// VueRouter.prototype.push = function push(location) {
+//   return originalPush.call(this, location).catch(err => err)
+// }
+
+const router =  new VueRouter({
+  routes
 })
+
+
+// 获取原型对象push函数
+const originalPush = VueRouter.prototype.push
+
+// 获取原型对象replace函数
+const originalReplace = VueRouter.prototype.replace
+
+// 修改原型对象中的push函数
+VueRouter.prototype.push = function push(location){
+return originalPush.call(this , location).catch(err=>err)
+}
+
+// 修改原型对象中的replace函数
+VueRouter.prototype.replace = function replace(location){
+return originalReplace.call(this , location).catch(err=>err)
+}
+
+
+export default router
